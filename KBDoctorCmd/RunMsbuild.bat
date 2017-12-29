8753335c4e6ee7a0bf6b4d26dd435857e7150bc1
nsanguinetti19 <nsanguinetti19@gmail.com>
2017-12-29 14:55:51 -0300

    NS: Ajustes para que funcione tarea msbuild. Signed-off-by: nsanguinetti19 <nsanguinetti19@gmail.com>

---
+++ KBDoctorCmd/RunMsbuild.bat
@@ -0,0 +1,4 @@
+rem set KBPath=""
+rem Completar con path de la KB
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe C:\Models\nsanguinetti\KBDoctor\KBDoctorCmd\KBDoctorCmd.msbuild /t:PrepareAndCompare
+pause
