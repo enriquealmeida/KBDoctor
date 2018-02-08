831a9e3d615ca40ecc9a269f1efe19f71ce96b6c
nsanguinetti19 <nsanguinetti19@gmail.com>
2018-02-08 10:47:59 -0300

    NS: Agrego cambio en .bat que deberia ser ignorado, pero no me deja hacer update.

--- KBDoctorCmd/RunMsbuild.bat
+++ KBDoctorCmd/RunMsbuild.bat
@@ -1,4 +1,4 @@
-set KBPath=C:\Models\gx15\Metricas_v15
+set KBPath=C:\Models\nsanguinetti\MT15v4
 rem Completar con path de la KB
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe C:\MisCosas\Proyectos\KBDoctorGIT\KBDoctor\KBDoctorCmd\KBDoctorCmd.msbuild /t:PrepareAndCompare
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe C:\Models\nsanguinetti\KBDoctor\KBDoctorCmd\KBDoctorCmd.msbuild /t:CleanAllObjectsVariables
 pause
