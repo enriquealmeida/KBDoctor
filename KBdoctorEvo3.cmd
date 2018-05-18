07bfd368c4b658eb35c1b1db29f3d7b912dadfc0
nsanguinetti19 <nsanguinetti19@gmail.com>
2018-05-18 13:47:04 -0300

    NS: Ajuste Bats Evo3 y 15

--- KBdoctorEvo3.cmd
+++ KBdoctorEvo3.cmd
@@ -1,3 +1,3 @@
 cd %~dp0
-setEnvKBDoctor EVO3
+call setEnvKBDoctor EVO3
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" KBDoctor.sln
--- KBdoctorGX15.cmd
+++ KBdoctorGX15.cmd
@@ -1,3 +1,4 @@
 cd %~dp0
-setenvKBDoctor GX15
+call setenvKBDoctor GX15
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" KBDoctor.sln
+pause
