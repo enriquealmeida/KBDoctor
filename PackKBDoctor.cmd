6c2deb354fb3ad0631c1d0bf9883da7a522410be
Enrique <ealmeida@concepto.com.uy>
2018-06-12 09:07:50 -0300

    EA - Enpaquedato de archivos

--- PackKBDoctor.cmd
+++ PackKBDoctor.cmd
@@ -2,12 +2,15 @@ if %1%a==a goto nada
     set targetDir=..\_KBDoctorPack\%1%
 	rmdir  /s /q %targetDir%
 	md %targetDir%
-	xcopy KBDoctorCmd\bin\debug\KBDoctorCmd.dll %targetDir%\Packages\
-	xcopy KBDoctorCore\bin\debug\KBDoctorCore.dll %targetDir%\Packages\
-	xcopy KBDoctorCore\IniFileParser.dll               %targetDir%
-	xcopy Louvain\bin\debug\LouvainCommunityPL.dll %targetDir%\Packages\
-	xcopy KBDoctorUI\bin\Debug\KBDoctor.dll  %targetDir%\Packages\
-	xcopy KBDoctorCmd\KBDoctorCmd.Tasks.targets %targetDir%\
+	xcopy KBDoctorCmd\bin\debug\KBDoctorCmd.dll %targetDir%
+	xcopy KBDoctorCore\bin\debug\KBDoctorCore.dll %targetDir%
+	xcopy IniFileParser\bin\debug\IniFileParser.dll %targetDir%
+	xcopy Louvain\bin\debug\LouvainCommunityPL.dll %targetDir%
+	xcopy KBDoctorUI\bin\Debug\KBDoctor.dll  %targetDir%
+	xcopy KBDoctorCmd\KBDoctorCmd.Tasks.targets %targetDir%
+
 :nada
-
-
+pushd %targetDir%
+"C:\Program Files\7-Zip\7z.exe" a %1%.zip  KBDoctorCmd.dll  KBDoctorCore.dll IniFileParser.dll LouvainCommunityPL.dll KBDoctor.dll KBDoctorCmd.Tasks.targets
+popd 
+pause
