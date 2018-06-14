d161aceb5a5d43ccb6bb85711a9f5c315aca92ab
Enrique <ealmeida@concepto.com.uy>
2018-06-14 17:44:10 -0300

    EA - ajustes para cambiar el csproj para que el run funcione en todos los casos.

--- KBdoctor_Build.cmd
+++ KBdoctor_Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-
+echo on
 call BuildOne EVO3
 call BuildOne GX15
 call BuildOne GX16
--- SetEnvKBDoctor.cmd
+++ SetEnvKBDoctor.cmd
@@ -8,6 +8,7 @@ SET GX_PROGRAM_DIR=C:\Program Files (x86)\Artech\GeneXus\GeneXusXEv3
 SET GX_SDK_DIR=C:\Program Files (x86)\Artech\GeneXus\GeneXusXEv3PlatformSDK
 SET NETFRAMEWORK_DIR="C:\Program Files (x86)\MSBuild\14.0\bin\amd64"
 SET TargetFrameworkVersion=v3.5
+ctt.exe source:KBDoctorUI\KBDoctorUI.csproj transform:KBDoctorUI\TransformacionEVO3.xml destination:KBDoctorUI\KBDoctorUI.csproj i
 GOTO END_CASE
 
 :CASE_GX15
@@ -15,6 +16,7 @@ SET GX_PROGRAM_DIR=C:\Program Files (x86)\GeneXus\GeneXus15
 SET GX_SDK_DIR=C:\Program Files (x86)\GeneXus\GeneXus15PlatformSDK
 SET NETFRAMEWORK_DIR="C:\Program Files (x86)\MSBuild\14.0\bin\amd64"
 SET TargetFrameworkVersion=v4.6
+ctt.exe source:KBDoctorUI\KBDoctorUI.csproj transform:KBDoctorUI\TransformacionGX15.xml destination:KBDoctorUI\KBDoctorUI.csproj i
 GOTO END_CASE
 
 :CASE_GX16
@@ -22,7 +24,7 @@ SET GX_PROGRAM_DIR=C:\Program Files (x86)\GeneXus\GeneXus16
 SET GX_SDK_DIR=C:\Program Files (x86)\GeneXus\GeneXus16PlatformSDK
 SET NETFRAMEWORK_DIR="C:\Program Files (x86)\MSBuild\14.0\bin\amd64"
 SET TargetFrameworkVersion=v4.7.1
-
+ctt.exe source:KBDoctorUI\KBDoctorUI.csproj transform:KBDoctorUI\TransformacionGX16.xml destination:KBDoctorUI\KBDoctorUI.csproj i
 GOTO END_CASE
 
 :DEFAULT_CASE
@@ -30,5 +32,5 @@ ECHO SetKBDoctor parametro invalido %1 : Debe ser EVO3, GX15 O GX16
 PAUSE
 
 :END_CASE
-  GOTO :EOF 
+ GOTO :EOF 
 
