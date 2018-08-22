ba6164ff9d21ca00b42c0c19bcde28bd7f0eccfb
Enrique <ealmeida@concepto.com.uy>
2018-08-22 10:45:04 -0300

    EA - Corrida Batch

--- KBDoctorCmd/RunMsbuild.cmd
+++ KBDoctorCmd/RunMsbuild.cmd
@@ -1,11 +1,17 @@
 cd ..
 call setEnvKBDoctor.cmd EVO3
 cd KBDoctorCmd
-set KBPath=C:\Models\ev3\LUCIAx_v81
+set KBPath=C:\Models\ev3\RiesgoDIAN
 
 del salida.txt
 echo %KBPath%  > salida.txt
-"%GX_PROGRAM_DIR%\GeneXus.exe" /install
-%NETFRAMEWORK_DIR%\MSBuild.exe KBDoctorCmd.msbuild /t:ReviewObjects >> salida.txt
+echo %GX_PROGRAM_DIR% >> SALIDA.TXT
+echo %NETFRAMEWORK_DIR% >> salida.txt
+echo %GX_SDK_DIR% >> salida.txt
+echo %TargetFrameworkVersion% >> salida.txt
+
+rem "%GX_PROGRAM_DIR%\GeneXus" /install
+
+%NETFRAMEWORK_DIR%\MSBuild.exe KBDoctorCmd.msbuild   /t:ReviewObjects /p:GX_PROGRAM_DIR="%GX_PROGRAM_DIR%" >> salida.txt
 notepad salida.txt
 pause
